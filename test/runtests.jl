f68957be120b257031a19a4db336327ec43e8162
Gus Hart <gus.hart@gmail.com>
2021-03-09 09:57:04 +0000

    Added several tests to 'runtests.jl' to improve coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using MinkowskiReduction
 using Test
+using Random
 using LinearAlgebra
 
 @testset "MinkowskiReduction.jl" begin
@@ -11,4 +12,9 @@ using LinearAlgebra
     @test orthogonalityDefect(fcc...)≈1.4142135623730954
     m = DeviousMat(26) # Largest size that doesn't overflow
     @test det(hcat(minkReduce(m[:,1],m[:,2],m[:,3])...))==1
+    @test all(MinkowskiReduction.shortenW_in_UVW(U,V,W) .≈ ([-2.0, 0.0, 0.0], [1.0, 2.0, 3.0], [-2.0, -2.0, 1.0]))
+    @test (m = DeviousMat(26); all(minkReduce(m[:,1],m[:,2],m[:,3],true) .≈ ([-1.0, 0.0, 0.0], [0.0, 0.0, -1.0], [0.0, -1.0, 0.0], 15)))
+    @test (m = DeviousMat(20); all(minkReduce(m[:,1],m[:,2],m[:,3],true) .≈ ([0.0, 0.0, -1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], 12)))
+    @test (Random.seed!(1234); MinkowskiReduction.RandLowerTri(35)==[1 0;-28 1])
+    @test (MinkowskiReduction.FibonacciMat(45)==[1836311903 2971215073; 1134903170 1836311903])
 end
