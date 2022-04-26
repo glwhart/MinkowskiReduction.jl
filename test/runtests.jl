73739be7833a2bc45abd283b50e52267d4ef6ffb
Gus Hart <gus.hart@gmail.com>
2022-04-26 14:30:49 -0600

    Disabled tests which relied on the RNG never changing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-#using MinkowskiReduction
+using MinkowskiReduction
 using Test
 using Random
 using LinearAlgebra
@@ -15,10 +15,10 @@ using LinearAlgebra
     @test all(MinkowskiReduction.shortenW_in_UVW(U,V,W) .≈ ([-2.0, 0.0, 0.0], [1.0, 2.0, 3.0], [-2.0, -2.0, 1.0]))
     @test (m = DeviousMat(26); all(minkReduce(m[:,1],m[:,2],m[:,3],true) .≈ ([-1.0, 0.0, 0.0], [0.0, 0.0, -1.0], [0.0, -1.0, 0.0], 15)))
     @test (m = DeviousMat(20); all(minkReduce(m[:,1],m[:,2],m[:,3],true) .≈ ([0.0, 0.0, -1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], 12)))
-    @test (Random.seed!(1234); MinkowskiReduction.RandLowerTri(35)==[1 0;-28 1])
+    #@test (Random.seed!(1234); MinkowskiReduction.RandLowerTri(35)==[1 0;-28 1])
     @test (MinkowskiReduction.FibonacciMat(45)==[1836311903 2971215073; 1134903170 1836311903])
-    @test (Random.seed!(1234);MinkowskiReduction.RandUnimodMat(4)==[-1 -1; 0 -1])
-    @test (Random.seed!(1234);MinkowskiReduction.RandUnimodMat(2)==[0 1; -1 -1])
+    #@test (Random.seed!(1234);MinkowskiReduction.RandUnimodMat(4)==[-1 -1; 0 -1])
+    #@test (Random.seed!(1234);MinkowskiReduction.RandUnimodMat(2)==[0 1; -1 -1])
     @test !(U = [1, 2, 9]; V = [-3, 4, 3]; W = [3, 0, 4]; isMinkReduced(U,V,W))
     @test !(U = [1, 2, 3]; V = [-3, 4, 3]; W = [3, 0, 4]; isMinkReduced(U,V,W))
     @test !(U = [1, 0, 0]; V = [-1, 0, 1]; W = [3, 0, 4]; isMinkReduced(U,V,W))
